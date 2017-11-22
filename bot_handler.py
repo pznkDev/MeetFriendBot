0849ad9e1b340b49675410dae8075e962e5a2657
Slava Kovalenko <oneforus7@gmail.com>
2017-11-22 19:52:21 +0200

    added "full"(without db) user_journey for finding people

--- bot_handler.py
+++ bot_handler.py
@@ -14,13 +14,9 @@ def handle_start(bot, message):
 
 def handle_find(bot, message):
     history[message.chat.id] = {
-        'state': bot_msg.STATE_FIND_START
+        'state': bot_msg.STATE_FIND_INPUT_AGE
     }
-    bot.send_message(message.chat.id, bot_msg.MSG_FIND_START, parse_mode='Markdown')
-
-
-def send_msg(bot, chat_id, msg):
-    bot.send_message(chat_id, msg, parse_mode='Markdown')
+    send_msg_find_input_age(bot, message)
 
 
 def handle_message(bot, message):
@@ -28,15 +24,15 @@ def handle_message(bot, message):
     state = history[message.chat.id]['state'] if message.chat.id in history else ''
     print('Current_state:', state)
 
-    if state == bot_msg.STATE_FIND_START:
+    if state == bot_msg.STATE_FIND_INPUT_AGE:
         try:
             age = int(message.text)
             if 0 < age < 6:
                 send_msg(bot, message.chat.id, 'WE ARE YOUNG')
             elif 6 <= age <= 100:
                 history[message.chat.id]['find_age'] = age
-                history[message.chat.id]['state'] = bot_msg.STATE_FIND_INPUT_AGE
-                print(history)
+                history[message.chat.id]['state'] = bot_msg.STATE_FIND_INPUT_SEX
+                send_msg_find_input_sex(bot, message)
             elif age > 100:
                 send_msg(bot, message.chat.id, 'Too old')
             else:
@@ -44,5 +40,45 @@ def handle_message(bot, message):
 
         except ValueError:
             send_msg(bot, message.chat.id, bot_msg.MSG_ERROR_AGE)
+
+    elif state == bot_msg.STATE_FIND_INPUT_SEX:
+        if message.text in ('male', 'female'):
+            history[message.chat.id]['find_sex'] = message.text
+            history[message.chat.id]['state'] = bot_msg.STATE_FIND_INPUT_LOCATION
+            send_msg_find_input_location(bot, message)
+        else:
+            send_msg(bot, message.chat.id, bot_msg.MSG_ERROR_SEX)
+
+    elif state == bot_msg.STATE_FIND_INPUT_LOCATION:
+        if message.text != '':
+            history[message.chat.id]['find_location'] = message.text
+            history[message.chat.id]['state'] = bot_msg.STATE_INIT
+            send_msg_find_start(bot, message)
+            # TODO "Find" request
+        else:
+            send_msg(bot, message.chat.id, bot_msg.MSG_ERROR_LOCATION)
+
     else:
         pass
+
+
+def send_msg_find_input_age(bot, message):
+    bot.send_message(message.chat.id, bot_msg.MSG_FIND_INPUT_START, parse_mode='Markdown')
+
+
+def send_msg_find_input_sex(bot, message):
+    markup_start = telebot.types.ReplyKeyboardMarkup(True, True)
+    markup_start.row('male', 'female')
+    bot.send_message(message.chat.id, bot_msg.MSG_FIND_INPUT_SEX, parse_mode='Markdown', reply_markup=markup_start)
+
+
+def send_msg_find_input_location(bot, message):
+    bot.send_message(message.chat.id, bot_msg.MSG_FIND_INPUT_LOCATION, parse_mode='Markdown')
+
+
+def send_msg_find_start(bot, message):
+    bot.send_message(message.chat.id, bot_msg.MSG_FIND_START, parse_mode='Markdown')
+
+
+def send_msg(bot, chat_id, msg):
+    bot.send_message(chat_id, msg, parse_mode='Markdown')
--- bot_messages.py
+++ bot_messages.py
@@ -1,11 +1,19 @@
 MSG_START = 'Hello, i\'ll help you to _find new friends_) Choose */login* to create account or */find* to look for ' \
             'some new friends '
+MSG_FIND_START = 'Okay, let me see... \n_(it can take a few seconds)_'
 
-MSG_FIND_START = 'Let\'s find somebody. But first, input *age*'
+MSG_FIND_INPUT_START = 'Let\'s find somebody. But first, input *age*'
+MSG_FIND_INPUT_SEX = '*male* or *female* ?'
+MSG_FIND_INPUT_LOCATION = 'Enter country(english)'
 
 MSG_ERROR_AGE = 'Sorry, but age is incorrect'
+MSG_ERROR_SEX = 'Sorry, but sex is incorrect'
+MSG_ERROR_LOCATION = 'Sorry, but location is incorrect'
+
 
 # states for Finite-state machine
+STATE_INIT = 'state_init'
+
 STATE_LOGIN_START = 'state_login_start'
 STATE_LOGIN_INPUT_NAME = 'state_login_input_name'
 STATE_LOGIN_INPUT_ID = 'state_login_input_id'
@@ -13,7 +21,6 @@ STATE_LOGIN_INPUT_AGE = 'state_login_input_age'
 STATE_LOGIN_INPUT_SEX = 'state_login_input_sex'
 STATE_LOGIN_INPUT_LOCATION = 'state_login_input_location'
 
-STATE_FIND_START = 'state_find_start'
 STATE_FIND_INPUT_AGE = 'state_find_input_age'
 STATE_FIND_INPUT_SEX = 'state_find_input_sex'
 STATE_FIND_INPUT_LOCATION = 'state_find_input_location'
