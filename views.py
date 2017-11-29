12d8eadd04d4dfaf370a0ba94250734d34db1360
nikita <fordf150svtraptor@gmail.com>
2017-11-29 23:21:40 +0200

    init db query and handler

--- views.py
+++ views.py
@@ -1,5 +1,14 @@
+import db
+
 from aiohttp import web
 
 
-async def main(request):
-    return web.Response(text='Lol')
+async def get_all_users(request):
+    async with request.app['db'].acquire() as conn:
+        try:
+            user_records = await db.get_all_users(conn)
+        except db.RecordNotFound as e:
+            raise web.HTTPNotFound(text=str(e))
+
+        users = [dict(u) for u in user_records]
+        return web.json_response({'users': users})
